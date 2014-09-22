e0d77172dfeb5f8a804e1b769ad56381df5c06a8
Katlyn Schwaebe <schwaebek@gmail.com>
2014-09-22 10:02:33 -0400

    FreeWithAd - basic ad setup

--- FreeWithAd/ViewController.swift
+++ FreeWithAd/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import iAd
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ADBannerViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //self.canDisplayBannerAds = true
+        
+        var bannerView = ADBannerView(adType: ADAdType.MediumRectangle)
+        bannerView.delegate = self
+        self.view .addSubview(bannerView)
+        
+        var bannerView1 = ADBannerView()
+        bannerView.center = CGPointMake(bannerView.center.x, UIScreen.mainScreen().bounds.size.height - (bannerView1.frame.size.height / 2.0))
+        bannerView1.delegate = self
+        self.view .addSubview(bannerView1)
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        
     }
 
     override func didReceiveMemoryWarning() {
